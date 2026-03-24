26d41b65bac234816d3eaa676303844b2a44b11c
Suhail <shab35360@gmail.com>
2026-03-24 12:51:32 +0530

    fix start file

--- server.js
+++ server.js
@@ -19,6 +19,8 @@ app.use("/api/auth", require("./routes/authRoutes"));
 app.use("/api/tasks", require("./routes/taskRoutes"));
 
 // server start
-app.listen(5000, () => {
-  console.log("Server running on port 5000");
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
