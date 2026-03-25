84a73711dc0899aeb42ae581e4dd91a55cc78106
Suhail <shab35360@gmail.com>
2026-03-25 12:07:49 +0530

    added start script

--- controllers/authController.js
+++ controllers/authController.js
@@ -2,7 +2,6 @@ const User = require("../models/User");
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 
-// Signup
 // signup
 exports.signup = async (req, res) => {
   const { name, email, password } = req.body;
